a3277526e8c7be6f5fff1c94adc510a4b0109fcc
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-04 00:38:57 +0530

    Roll back header logo to file.svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -95,7 +95,7 @@ export default function Header({ userEmail, onLogout }: HeaderProps) {
         {/* Centered VA Logo */}
         <div className="flex flex-col items-center justify-center flex-1">
           <img 
-            src="/file(1).svg" 
+            src="/file.svg" 
             alt="Villa Altona Logo"
             width="80" 
             height="60" 
