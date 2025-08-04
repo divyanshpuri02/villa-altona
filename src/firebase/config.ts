7a34dab0ebcc50ea824f092c9209b2e9b50e7a0e
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 04:44:44 +0530

    Fix booking system and database connectivity

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getFunctions } from 'firebase/functions';
@@ -7,17 +6,15 @@ import { getFunctions } from 'firebase/functions';
 const firebaseConfig = {
   apiKey: "AIzaSyDpY1J38Mf5V-XC3U3CgwlBT_mNexdYM2o",
   authDomain: "villa-altona-goa.firebaseapp.com",
-  databaseURL: "https://villa-altona-goa-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "villa-altona-goa",
   storageBucket: "villa-altona-goa.firebasestorage.app",
   messagingSenderId: "686251298348",
-  appId: "1:686251298348:web:d801992f8c1bbc1918cb89",
-  measurementId: "G-XY8PM1G620"
+  appId: "1:686251298348:web:d6a0c708b19556fd18cb89",
+  measurementId: "G-M6B1E4HFGP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const functions = getFunctions(app, 'asia-south1');
