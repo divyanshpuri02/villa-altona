416ce0ca96a23c1cf9619598e5b2e2d0faff5aa9
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 00:33:02 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -126,8 +126,7 @@ export default function Header({ userEmail, onLogout }: HeaderProps) {
                     srcset="/public/file1.svg"></img>
                 
                  
-                <div className="h-0.5 w-full bg-gradient-to-r from-transparent via-white to-transparent opacity-60 mt-1"></div>
-              </div>
+               
             
 
             {/* User Menu - Right - Now in main div */}
