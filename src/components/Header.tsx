4f321b76b8643b81ddbe19dba3cc025074437870
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 00:30:49 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,7 +121,7 @@ export default function Header({ userEmail, onLogout }: HeaderProps) {
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <div className="text-center">
                
-                  <img height="150" width="150" transform="scale(200000)"
+                  <img height="150" width="150" transform="blur(200000)"
                     src="/public/file1.svg" 
                     srcset="/public/file1.svg"></img>
                 
