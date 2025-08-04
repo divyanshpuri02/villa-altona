9c6cab1939e8376a38f59a040345e1dcf07ef9c8
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 01:24:42 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
           {/* Logo - Centered */}
           <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div className="text-center">
-              <h1 >
+              <h1 className="text-2xl font-bold text-black tracking-loose" style={{ fontFamily: '"Noto Serif", serif' }}>
                 <img
                   src="/file1.svg" 
                   alt="Villa Altona" 
