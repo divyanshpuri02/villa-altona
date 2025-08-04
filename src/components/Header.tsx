5138ad9734d669cc8d6a5395c024b4da98d200b0
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 00:58:06 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,7 +51,7 @@ const Header = () => {
                 <img
                   src="/file.svg" 
                   alt="Villa Altona" 
-                  className="h-15 w-auto"
+                  className="h-10 w-auto"
                   onError={(e) => {
                     e.currentTarget.style.display = 'none';
                     e.currentTarget.nextElementSibling!.style.display = 'inline';
