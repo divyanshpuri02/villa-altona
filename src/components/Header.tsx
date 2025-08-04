a3dee3db180a13d156aa3f2a8cf6265f756a4ac5
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 00:31:16 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -120,7 +120,7 @@ export default function Header({ userEmail, onLogout }: HeaderProps) {
             {/* Centered Logo - Now in main div */}
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <div className="text-center">
-                  <img height="150" width="150" transform="blur(200000)"
+                  <img height="150" width="150" transform="blur(20)"
                     src="/public/file1.svg" 
                     srcset="/public/file1.svg"></img>
                 
