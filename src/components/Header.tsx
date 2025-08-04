0fe2251bb3c5f83c3629f62512e02f8535b99f34
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 00:07:56 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -120,10 +120,10 @@ export default function Header({ userEmail, onLogout }: HeaderProps) {
             {/* Centered Logo - Now in main div */}
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <div className="text-center">
-                <iframe src="triangle.svg" width="500" height="500">
+               
                   <img height="150" width="150" transform="scale(20)"
                     src="/public/file1.svg" 
-                    srcset="/public/file1.svg"></img></iframe>
+                    srcset="/public/file1.svg"></img>
                 
                  
                 <div className="h-0.5 w-full bg-gradient-to-r from-transparent via-white to-transparent opacity-60 mt-1"></div>
