2ca14ebcee4a24fb1656736c5f922988204ddb54
divyanshpuri02 <divyanshpuri02@gmail.com>
2025-08-05 01:33:13 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -84,7 +84,7 @@ const Header = () => {
                   </div>
                   <button
                     onClick={handleSignOut}
-                    className="flex items-center w-full px-4 py-4 text-sm text-black hover:bg-white/20 text-red transition-colors duration-200"
+                    className="flex items-center w-full px-2 py-2 text-sm text-black hover:bg-white/20 text-red transition-colors duration-200"
                   >
                     <LogOut size={16} className="mr-2" />
                     Sign out
